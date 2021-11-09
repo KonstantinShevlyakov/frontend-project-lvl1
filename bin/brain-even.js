da5c4cadaf6a19eeb373003c33b60871ad161759
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2021-11-09 22:00:25 +0300

    brain-even completed

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const brainEvenGame = () => {
-    let count = 0;
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
-    while (count < 3) {
-       const num = random(1, 100);
-       const question = readlineSync.question('Question: ' + num + '\nYour answer:');
-       if (((num % 2 === 0) && question === 'yes') || ((num % 2 !== 0) && question === 'no')) {
-           console.log('Correct!');
-           count += 1;
-       } else {
-           console.log(`"${question}" is wrong answer ;(. Correct answer was "${num % 2 ? 'no' : 'yes'}".\nLet's try again, ${userName}!`);
-           count = 0;
-       }
+  let count = 0;
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
+  while (count < 3) {
+    const num = random(1, 100);
+    const question = readlineSync.question(`Question: ${num}\nYour answer:`);
+    if (((num % 2 === 0) && question === 'yes') || ((num % 2 !== 0) && question === 'no')) {
+      console.log('Correct!');
+      count += 1;
+    } else {
+      console.log(`"${question}" is wrong answer ;(. Correct answer was "${num % 2 ? 'no' : 'yes'}".\nLet's try again, ${userName}!`);
+      count = 0;
     }
-    return (`Congratulations,${userName}`);
+  }
+  return (`Congratulations,${userName}!`);
 };
 console.log(brainEvenGame());
