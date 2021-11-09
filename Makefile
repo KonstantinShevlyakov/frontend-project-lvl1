02dabbe6a26030cf32e4d7c369b68abf37a61a14
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2021-11-09 17:28:28 +0300

    eslint

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ brain-games: # запуск проект
 	node bin/brain-games.js
 publish: # публикация пакета
 	npm publish --dry-run
+lint: # запуск eslint
+	npx eslint .
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const question = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return ('Hello ' + userName + '!');
+const question = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return (`Hello ${userName}!`);
 };
+
+export default question();
