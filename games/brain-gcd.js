eaaf2cce6da780679eec82de59035963106b8f45
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2021-11-11 15:21:14 +0300

    random 0-100

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,24 +1,24 @@
 import {
-    getGCD,
-    greeting, mainQuestion, playerLost, playerWin, random
+  getGCD,
+  greeting, mainQuestion, playerLost, playerWin, random,
 } from '../src/index.js';
 
 const brainGCDGame = () => {
-    let count = 0;
-    const userName = greeting();
-    while (count < 3) {
-        const num1 = random(0, 100);
-        const num2 = random(0, 100);
-        const correctResult = getGCD(num1, num2);
-        const question = Number(mainQuestion(`${num1} ${num2}`));
-        if (question === correctResult) {
-            console.log('Correct!');
-            count += 1;
-        } else {
-            console.log(playerLost(question, correctResult, userName));
-            count = 0;
-        }
+  let count = 0;
+  const userName = greeting();
+  while (count < 3) {
+    const num1 = random(0, 100);
+    const num2 = random(0, 100);
+    const correctResult = getGCD(num1, num2);
+    const question = Number(mainQuestion(`${num1} ${num2}`));
+    if (question === correctResult) {
+      console.log('Correct!');
+      count += 1;
+    } else {
+      console.log(playerLost(question, correctResult, userName));
+      count = 0;
     }
-    return playerWin(userName);
+  }
+  return playerWin(userName);
 };
 console.log(brainGCDGame());
