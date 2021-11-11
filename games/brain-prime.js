80dd1595ff3adbe330ec8869c37d4fad7e1d62c2
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2021-11-12 00:41:21 +0300

    brain-prime completed

---
+++ games/brain-prime.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import {
+  greeting, mainQuestion, playerLost, playerWin, random, isPrime,
+} from '../src/index.js';
+
+const brainPrimeGame = () => {
+  let count = 0;
+  const userName = greeting();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  while (count < 3) {
+    const num = random(1, 100);
+    const correctResult = isPrime(num);
+    const question = (mainQuestion(`${num}`));
+    if (question === correctResult) {
+      console.log('Correct!');
+      count += 1;
+    } else {
+      console.log(playerLost(question, correctResult, userName));
+      count = 0;
+    }
+  }
+  return playerWin(userName);
+};
+console.log(brainPrimeGame());
