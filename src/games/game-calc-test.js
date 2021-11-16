bbf1423c8d62ee7744667db5a3e70aa32ecfc958
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2021-11-17 01:05:22 +0300

    new logic game-calc

---
+++ src/games/game-calc-test.js
@@ -0,0 +1,26 @@
+import { random } from '../index-test.js';
+
+const announcement = 'What is the result of the expression?';
+const questions = [];
+const correctResults = [];
+const operators = ['-', '*', '+'];
+for (let i = 0; i < 3; i += 1) {
+    const num1 = random(0, 10);
+    const num2 = random(0, 10);
+    const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+    console.log(randomOperator);
+    questions.push(`${num1} ${randomOperator} ${num2}`);
+    switch (randomOperator) {
+        case '-':
+            correctResults.push(String(num1 - num2));
+            break;
+        case '+':
+            correctResults.push(String(num1 + num2));
+            break;
+        case '*':
+            correctResults.push(String(num1 * num2));
+            break;
+    }
+};
+
+export { announcement, questions, correctResults };
