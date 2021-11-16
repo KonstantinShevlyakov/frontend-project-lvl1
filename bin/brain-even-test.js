bd3ef16888154c54126769d3944b6d4995c07c88
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2021-11-16 22:18:47 +0300

    test files logic changed

--- bin/brain-even-test.js
+++ bin/brain-even-test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { greeting, userName } from '../src/cli.js';
-import gameProperties from '../src/index-test.js';
+import { gameProperties } from '../src/index-test.js';
 import { announcement, questions, correctResults } from '../src/games/game-even-test.js';
 
 greeting();
