31e4d2f6f5430fae00a15ee0d1f893132e8c29ec
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-01-11 22:29:45 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ export default function gameProperties(announcement, greeting, rounds, gameData)
   const name = greeting();
   console.log(announcement);
   for (let i = 0; i < rounds; i += 1) {
-    // const question = Object.keys(questionsResults)[i];
     console.log(`Question: ${gameData[i].question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === gameData[i].answer) {
