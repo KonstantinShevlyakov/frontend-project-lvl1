a77292668d576b225fe773ab3dff89a88fe5dbcf
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-01-16 20:27:54 +0300

    prime changed

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import greeting from '../src/cli.js';
-import gameProperties from '../src/index.js';
-import {
-  announcement, rounds, gameData,
-} from '../src/games/game-prime.js';
+// import greeting from '../src/cli.js';
+// import gameProperties from '../src/index.js';
+// import {
+//   announcement, rounds, gameData,
+// } from '../src/games/game-prime.js';
+import prime from '../src/games/game-prime.js';
 
-gameProperties(announcement, greeting, rounds, gameData);
+prime();
--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,30 +1,35 @@
 import random from '../randomizer.js';
+import gameProperteis from '../index.js';
+import { roundsCount } from '../index.js';
 
-const isPrime = (num) => {
-  const smallestDivisor = (numeral) => {
-    let counter = 2;
-    for (; numeral % counter !== 0;) {
-      counter += 1;
+export default function prime() {
+  const isPrime = (num) => {
+    const smallestDivisor = (numeral) => {
+      let counter = 2;
+      for (; numeral % counter !== 0;) {
+        counter += 1;
+      }
+      return counter;
+    };
+    if (num >= 1 && smallestDivisor(num) === num) {
+      return true;
     }
-    return counter;
+    return false;
   };
-  if (num >= 1 && smallestDivisor(num) === num) {
-    return true;
-  }
-  return false;
-};
 
-const announcement = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const gameData = [];
-const rounds = 3;
-for (let i = 0; i < rounds; i += 1) {
-  const questionResults = {};
-  const num = random(1, 100);
-  isPrime(num) ? questionResults.answer = 'yes' : questionResults.answer = 'no';
-  questionResults.question = num;
-  gameData.push(questionResults);
+  const announcement = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const gameData = [];
+  const rounds = roundsCount();
+  for (let i = 0; i < rounds; i += 1) {
+    const questionResults = {};
+    const num = random(1, 100);
+    isPrime(num) ? questionResults.answer = 'yes' : questionResults.answer = 'no';
+    questionResults.question = num;
+    gameData.push(questionResults);
+  }
+  return gameProperteis(announcement, gameData);
 }
 
-export {
-  announcement, gameData, rounds,
-};
+// export {
+//   announcement, gameData, rounds,
+// };
