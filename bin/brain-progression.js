75478f5de12ca5635be1816c170d4c0975f3eef7
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-01-16 20:30:57 +0300

    progression changed

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-import greeting from '../src/cli.js';
-import gameProperties from '../src/index.js';
-import {
-  announcement, rounds, gameData,
-} from '../src/games/game-progression.js';
+// import greeting from '../src/cli.js';
+// import gameProperties from '../src/index.js';
+// import {
+//   announcement, rounds, gameData,
+// } from '../src/games/game-progression.js';
+import progression from '../src/games/game-progression.js';
 
-gameProperties(announcement, greeting, rounds, gameData);
+progression();
--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,27 +1,32 @@
 import random from '../randomizer.js';
+import gameProperties from '../index.js';
+import { roundsCount } from '../index.js';
 
-const announcement = 'What number is missing in the progression?';
-const gameData = [];
-const rounds = 3;
-for (let i = 0; i < rounds; i += 1) {
-  const questionResult = {};
-  const progression = [];
-  const progressionLength = random(5, 10);
-  const progStep = random(2, 10);
-  let element = random(0, 50);
-  progression.push(element);
-  for (let j = 0; j < progressionLength; j += 1) {
-    element += progStep;
+export default function progression() {
+  const announcement = 'What number is missing in the progression?';
+  const gameData = [];
+  const rounds = 3;
+  for (let i = 0; i < rounds; i += 1) {
+    const questionResult = {};
+    const progression = [];
+    const progressionLength = random(5, 10);
+    const progStep = random(2, 10);
+    let element = random(0, 50);
     progression.push(element);
+    for (let j = 0; j < progressionLength; j += 1) {
+      element += progStep;
+      progression.push(element);
+    }
+    const randomElement = progression[random(0, progressionLength - 1)];
+    questionResult.answer = randomElement.toString();
+    const index = progression.indexOf(randomElement);
+    progression[index] = '..';
+    questionResult.question = (progression.toString().split(',').join(' '));
+    gameData.push(questionResult);
   }
-  const randomElement = progression[random(0, progressionLength - 1)];
-  questionResult.answer = randomElement.toString();
-  const index = progression.indexOf(randomElement);
-  progression[index] = '..';
-  questionResult.question = (progression.toString().split(',').join(' '));
-  gameData.push(questionResult);
+  return gameProperties(announcement, gameData);
 }
 
-export {
-  announcement, rounds, gameData,
-};
+// export {
+//   announcement, rounds, gameData,
+// };
